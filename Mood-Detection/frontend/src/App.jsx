370b7218b7e7c8820ef32f3d4c54aaaff0b339af
moabia1 <mdmoabia81@gmail.com>
2025-07-18 23:34:57 +0530

    frontend work

---
+++ Mood-Detection/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import FacialExpression from "./components/FacialExpression";
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <FacialExpression />
+    </div>
+  );
+}
+
+export default App;
