a760228c74731a620996564ea612eb8867f9dd4e
moabia1 <mdmoabia81@gmail.com>
2025-08-18 14:56:04 +0530

    implement socket.io

--- chat-gpt/src/routes/chat.route.js
+++ chat-gpt/src/routes/chat.route.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const authUser = require("../middlewares/auth.middleware");
+const chatController = require("../controllers/chat.controller");
 
 const router = express.Router();
 
-router.get("/", authUser,(req, res) => {
-  res.status(201).json({
-    message: "Route Access Succesfull"
-  })
-})
+router.post("/", authUser, chatController)
 
 
 module.exports = router;
--- chat-gpt/src/sockets/socket.server.js
+++ chat-gpt/src/sockets/socket.server.js
@@ -1,15 +1,26 @@
 const { Server } = require("socket.io");
 const cookie = require("cookie")
-
+const jwt = require("jsonwebtoken")
+const userModel = require("../models/user.model")
+  
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
 
-  io.use((socket, next) => {
-    const cookies = cookie.parse(socket.handshake.headers?.cookie || "")
+  io.use(async(socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
 
     if (!cookies.token) {
-      next(new Error ("Authentication error (No Token Provided)"))
-     }
+      next(new Error("Authentication error (No Token Provided)"));
+    }
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+      const user = await userModel.findById(decoded.id)
+      socket.user = user
+      console.log(user)
+      next()
+    } catch (error) {
+      next(new Error("Authentication error (No Token Provided)"));
+    }
   });
 
   io.on("connection", (socket) => {
