098a976ead84bb15a03589737a0392d9b944aff2
moabia1 <mdmoabia81@gmail.com>
2025-08-21 10:55:16 +0530

    implement pinecone for vector database

---
+++ chat-gpt/src/services/vector.service.js
@@ -0,0 +1,25 @@
+// Import the Pinecone library
+const { Pinecone } = require("@pinecone-database/pinecone");
+
+// Initialize a Pinecone client with your API key
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY});
+
+// Create a dense index with integrated embedding
+const chatGptIndex = pc.index("chat-gpt");
+
+async function createMemory({vectors, metadata,messageId}) {
+  await chatGptIndex.upsert([{
+    id: messageId,
+    values: vectors,
+    metadata
+  }])
+}
+
+async function queryMemory({ queryVector, limit = 5, metadata }) {
+  const data = await chatGptIndex.query({
+    vector: queryVector,
+    topK: limit,
+    filter: metadata ? { metadata } : undefined,
+    
+  })
+}
